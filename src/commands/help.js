c27945da38edb16b1b901f54ed39db2e87f72c35
Aryuko <pachimaryu@gmail.com>
2018-09-25 17:46:20 +0200

    Fix crash when no argument is supplied to !help

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,14 +4,24 @@ module.exports = new Command (
 	"help",
 	(message, input, client) => 
 	{
-		let command = client.Commands[input.args[0]];
-		
-		let responseEmbed = new client.Discord.RichEmbed()
-		.setColor(client.Config.colours.green)
-		.setTitle(client.Config.commandPrefix + command.commandName)
-		.addField("Description", command.description)
-		.addField("Syntax", "``" + command.syntax + "``");
-		message.channel.send(responseEmbed);
+		if (input.args.length > 0)
+		{
+			let command = client.Commands[input.args[0]];
+			
+			let responseEmbed = new client.Discord.RichEmbed()
+			.setColor(client.Config.colours.green)
+			.setTitle(client.Config.commandPrefix + command.commandName)
+			.addField("Description", command.description)
+			.addField("Syntax", "``" + command.syntax + "``");
+			message.channel.send(responseEmbed);
+		} else 
+		{
+			let responseEmbed = new client.Discord.RichEmbed()
+			.setColor(client.Config.colours.red)
+			.setTitle("Error")
+			.setDescription("Invalid supplied arguments, please follow the following syntax:\n``" + client.Config.commandPrefix + client.Commands['help'].syntax + "``");
+			message.channel.send(responseEmbed);
+		}
 	},
 	[],
 	"Helps by providing information about a given command.",
