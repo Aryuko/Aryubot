7ab8d509a9322e0dd64db0feb7c956c972bf6123
Aryuko <peter.kjellen@gmail.com>
2018-10-08 20:56:23 +0200

    Implement set, implement get for top level props

--- src/Config.js
+++ src/Config.js
@@ -9,30 +9,59 @@ class Config
 {
 	constructor ()
 	{
-		var validator = {
+		var handler = {
 			get: function(target, name)
 			{
-				if (!(name in target))
+				if(name in target)					// Top level call, like functions or the actual arrays of config
 				{
-					console.log("Getting non-existant property '" + name + "'");
-					return undefined;
-				} else if ((name in target) && typeof target[name] === 'object')  
-				{
-					console.log("Getting object property '" + name + "'");
-					return new Proxy(target[name], validator);
-				} else 
-				{
-					console.log("Getting existant property '" + name + "'");
 					return target[name];
+				} else								// Not found on the top level, you're probably looking for one of the config props
+				{									// Let's have a look in config
+					let config = target.config;
+					if (name in config)				// Cool! We found it :D
+					{
+						return config[name]
+					} else							// Hmm, could be in defaultConfig
+					{
+						let defaultConfig = target.defaultConfig;
+						if (name in defaultConfig)	// Ah, there it was! :D
+						{
+							return defaultConfig[name];
+						}
+						else 						// T_T
+						{
+							console.log("Tried reaching undefined property '" + name + "'");
+							return undefined;
+						}
+					}
+
+
+					/* if (!(name in config))
+					{
+						console.log("Getting non-existant property '" + name + "'");
+						return undefined;
+					} else if ((name in config) && typeof config[name] === 'object')  
+					{
+						console.log("Getting object property '" + name + "'");
+						return new Proxy(config[name], handler);
+					} else 
+					{
+						console.log("Getting existant property '" + name + "'");
+						return config[name];
+					} */
 				}
 			},
 			set: function(target, name, value)
 			{
-				if (!(name in target))
+				if (name in target)	// Top level call, like functions or the actual arrays
 				{
-					console.log("Setting non-existant property '" + name + "', initial value: " + value);
+					target[name] = value;
+
+				} else				// Not found on the top level, you probably want to set a config prop
+				{
+					target.config[name] = value;
+					target.save();
 				}
-				target[name] = value;
 				return true;
 			}
 		}
@@ -40,12 +69,13 @@ class Config
 		// Todo: Todo: Build custom config (from file or create if not found), include defaults in Config.Defaults // 
 		this.defaultConfig = defaultConfigFile;
 		this.config = {};
-		this.load().then(() => { return new Proxy(this, validator) });
+		this.load();
+		return new Proxy(this, handler);
 	}
 	
 	load () 
 	{
-		return new Promise (() => 
+		return new Promise ((resolve, reject) => 
 		{
 			try
 			{
@@ -60,12 +90,17 @@ class Config
 					this.config = configFile;
 				});
 			}
+			resolve();
 		})
 	}
 
 	save () 
 	{
-		fs.writeFile(absoluteConfigFilePath, JSON.stringify(this.config), (err) => console.log(err))
+		return new Promise ((resolve, reject) => 
+		{
+			fs.writeFile(absoluteConfigFilePath, JSON.stringify(this.config), (err) => { if (err) { console.log(err); } });
+			resolve();
+		})
 	}
 
 	getProperty (prop)
